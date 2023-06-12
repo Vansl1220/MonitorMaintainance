36429b081f230d667936afc75ffb522a027bcd5a
Vansl1220 <wi22b110@technikum-wien.at>
2023-06-12 19:15:05 +0200

    Initial commit

---
+++ src/main/java/com/example/monitormaintainance/MonitorMaintainanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.monitormaintainance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorMaintainanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorMaintainanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/monitormaintainance/MonitorMaintainanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monitormaintainance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorMaintainanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
